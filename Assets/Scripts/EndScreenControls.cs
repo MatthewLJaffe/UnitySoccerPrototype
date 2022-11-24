14049ff64718fbddfcc6e68e1ef268f902056cd7
Yimeng Liu <78622368+lyimeng@users.noreply.github.com>
2022-11-23 20:29:35 -0500

    UI Update

--- Assets/Scripts/EndScreenControls.cs
+++ Assets/Scripts/EndScreenControls.cs
@@ -1,19 +1,81 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using TMPro;
 
 public class EndScreenControls : MonoBehaviour
 {
+    
+    public UnityEvent onAnalysis;
+    public UnityEvent onEndMenu;
+    private bool _endBegin = true;
+    private bool _AnalysisBegin;
+    private int menuChoice = 0;
+    private int picChoice = 0;
+    [SerializeField] private TextMeshProUGUI retryText;
+    [SerializeField] private TextMeshProUGUI analysisText;
+    [SerializeField] private TextMeshProUGUI quitText;
+    public GameObject[] Images;
     private void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Application.Quit();
+        if (_endBegin) {
+            onEndMenu.Invoke();
+            if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                menuChoice += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                menuChoice -= 1;
+            }
+            
+            menuChoice = menuChoice % 4;
+            if (menuChoice == 0) {
+                retryText.fontStyle = (FontStyles)FontStyle.Bold;
+                analysisText.fontStyle = (FontStyles)FontStyle.Normal;
+                quitText.fontStyle = (FontStyles)FontStyle.Normal;
+                if (Input.GetButtonDown("Jump")) {
+                    SceneManager.LoadScene(0);
+                }
+            }
+            if (menuChoice == 1) {
+                retryText.fontStyle = (FontStyles)FontStyle.Normal;
+                analysisText.fontStyle = (FontStyles)FontStyle.Bold;
+                quitText.fontStyle = (FontStyles)FontStyle.Normal;
+                
+                if (Input.GetButtonDown("Jump")) {
+                    onAnalysis.Invoke();
+                    _endBegin = false;
+                    _AnalysisBegin = true;
+                }
+            }
+            if (menuChoice == 2) {
+                retryText.fontStyle = (FontStyles)FontStyle.Normal;
+                analysisText.fontStyle = (FontStyles)FontStyle.Normal;
+                quitText.fontStyle = (FontStyles)FontStyle.Bold;
+                if(Input.GetButtonDown("Jump")) {
+                    Application.Quit();
+                }
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(0);
+        
+        Debug.Log(_AnalysisBegin);
+        if (_AnalysisBegin) {
+            if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                Images[picChoice].SetActive(false);
+                picChoice += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                Images[picChoice].SetActive(false);
+                picChoice -= 1;
+            }
+            picChoice = picChoice % 3;
+            Images[picChoice].SetActive(true);
+            
+            if (Input.GetKeyDown(KeyCode.N)) {
+                _AnalysisBegin = false;
+                _endBegin = true;
+                onEndMenu.Invoke();
+            }
         }
     }
 }
--- Assets/Scripts/TransitionController.cs
+++ Assets/Scripts/TransitionController.cs
@@ -3,25 +3,115 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 
 public class TransitionController : MonoBehaviour
 {
     public UnityEvent onCountDownStart;
+    public UnityEvent onOptionStart;
+    public UnityEvent onMenuStart;
     public UnityEvent onGameStart;
     public static Action gameStart = delegate { };
     private bool _gameBegin;
+    private bool _optionBegin;
+    private bool _menuBegin = true;
     [SerializeField] private TextMeshProUGUI text;
-    
+    public Slider speedSlider;
+    public Slider volumeSlider;
+    [SerializeField] private TextMeshProUGUI speedSliderText;
+    [SerializeField] private TextMeshProUGUI volumeSliderText;
+    [SerializeField] private TextMeshProUGUI menuOptionText;
+    [SerializeField] private TextMeshProUGUI menuStartText;
+    [SerializeField] private TextMeshProUGUI menuQuitText;
+    private int sliderChoice = 0;
+    private int menuChoice = 0;
 
     private void Update()
     {
         //check for pressing space key
-        if (!_gameBegin && Input.GetButtonDown("Jump"))
-        {
-            onCountDownStart.Invoke();
-            _gameBegin = true;
-            StartCoroutine(CountDown());
+        // if (!_gameBegin && Input.GetButtonDown("Jump"))
+        // {
+        //     onCountDownStart.Invoke();
+        //     _gameBegin = true;
+        //     StartCoroutine(CountDown());
+        // }
+        
+        if (_menuBegin) {
+            if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                menuChoice += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                menuChoice -= 1;
+            }
+            menuChoice = menuChoice % 3;
+            if (menuChoice == 0) {
+                menuOptionText.fontStyle = (FontStyles)FontStyle.Bold;
+                menuStartText.fontStyle = (FontStyles)FontStyle.Normal;
+                menuQuitText.fontStyle = (FontStyles)FontStyle.Normal;
+                // option menu
+                if (Input.GetButtonDown("Jump")) {
+                    _menuBegin = false;
+                    _optionBegin = true;
+                    onOptionStart.Invoke();
+                }
+            }
+            if (menuChoice == 1) {
+                menuOptionText.fontStyle = (FontStyles)FontStyle.Normal;
+                menuStartText.fontStyle = (FontStyles)FontStyle.Bold;
+                menuQuitText.fontStyle = (FontStyles)FontStyle.Normal;
+                
+                if (!_gameBegin && Input.GetButtonDown("Jump")) {
+                    _menuBegin = false;
+                    onCountDownStart.Invoke();
+                    _gameBegin = true;
+                    StartCoroutine(CountDown());
+                }
+            }
+            if (menuChoice == 2) {
+                menuOptionText.fontStyle = (FontStyles)FontStyle.Normal;
+                menuStartText.fontStyle = (FontStyles)FontStyle.Normal;
+                menuQuitText.fontStyle = (FontStyles)FontStyle.Bold;
+                if(Input.GetButtonDown("Jump")) {
+                    Application.Quit();
+                }
+            }
+        }
+
+        // back to main menu
+        if (Input.GetKeyDown(KeyCode.B)) {
+            onMenuStart.Invoke();
+            _optionBegin = false;
+            _menuBegin = true;
+        }
+
+        if (_optionBegin) {
+            Slider slider;
+            if (sliderChoice == 0) {
+                slider = speedSlider;
+                speedSliderText.fontStyle = (FontStyles)FontStyle.Bold;
+                volumeSliderText.fontStyle = (FontStyles)FontStyle.Normal;
+            }
+            else {
+                slider = volumeSlider;
+                speedSliderText.fontStyle = (FontStyles)FontStyle.Normal;
+                volumeSliderText.fontStyle = (FontStyles)FontStyle.Bold;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow)) {
+                sliderChoice += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow)) {
+                sliderChoice -= 1;
+            }
+            sliderChoice = sliderChoice % 2;
+
+            if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                slider.value += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                slider.value -= 1;
+            }
+
         }
     }
 
